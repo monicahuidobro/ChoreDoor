daf7cd3839473477b9b40bd840edc8bedf1fe0b1
monicahuidobro <monicahuidobro@hotmail.com>
2023-01-02 09:46:12 +0100

    MEJORAS IMPLEMENTADAS - PROYECTO FINALIZADO

--- script.js
+++ script.js
@@ -1,105 +1,129 @@
-let doorImage1 = document.getElementById("door1");
-let doorImage2 = document.getElementById("door2");
-let doorImage3 = document.getElementById("door3");
+// ##VARIABLE DECLARATION
+//EXTRACT VARIABLES FROM index.html
+let door1 = document.getElementById("door1");
+let door2 = document.getElementById("door2");
+let door3 = document.getElementById("door3");
+
 let startButton = document.getElementById("start");
 
-let numClosedDoors = 3;
+let currentScore = document.getElementById("current");
+let bestScore = document.getElementById("best");
 
-const images = ["images/robot.svg", "images/beach.svg", "images/space.svg"];
-const closedImage = "file:///C:/Users/71215/Desktop/WEB/ChoreDoor/images/closed_door.svg"; 
-const botImage = "file:///C:/Users/71215/Desktop/WEB/ChoreDoor/images/robot.svg"
-
+// NEW VARIABLE DECLARATIONS
 let openDoor1;
 let openDoor2;
 let openDoor3;
 
 let currentlyPlaying =true;
-let currentScore = document.getElementById("current");
 let currentNum = 0;
+let bestNum = 0;
+let clicked1 = 0;
+let clicked2 = 0;
+let clicked3 = 0;
 
+let numClosedDoors = 3;
+
+const images = ["images/robot.svg", "images/beach.svg", "images/space.svg", "images/closed_door.svg"];
+
+// ## FUNCTIONS
+
+//RANDOM NUMBER GENERATOR TO CHOOSE THE IMAGE TO SHOW AFTER CLICKING ON EACH DOOR
 randomChoreDoorGenerator = () => {
   var choreDoor = Math.floor(Math.random()*numClosedDoors);
   switch (choreDoor){
     case 0:
       openDoor1 = images[0];
+      door1.value = "bot";
       openDoor2 = images [1];
+      door2.value = "open";
       openDoor3 = images [2];
+      door3.value = "open";
       break;
     case 1:
       openDoor1 = images[1];
+      door1.value = "open";
       openDoor2 = images [0];
+      door2.value = "bot";
       openDoor3 = images [2];
+      door3.value = "open" ;
       break;
     case 2:
       openDoor1 = images[2];
+      door1.value = "open";
       openDoor2 = images [1];
+      door2.value = "open";
       openDoor3 = images [0];
+      door3.value = "bot";
       break;
   }
 }
-
-randomChoreDoorGenerator();
-
-isClicked = (door) => {
-  if (door.src === closedImage) {
-    return false;
-  } else { return true; }
-}
-
-isBot = (door) => {
-  if (door.src === botImage){
-    return true;
-  } else { return false;}
-}
-
+// TO INDICATE WHEN THE USER HAS LOST
 gameOver = (status) => {
   if (status === "win"){
     startButton.innerHTML = "You win! Play again?";
+    currentNum ++;
+    bestNum = currentNum; 
   } else if (status === "lose") {
     startButton.innerHTML = "Game over! Play again?";
+    currentNum = 0;
   }
   currentlyPlaying = false;
 }
-
+// TO CONTROL WHEN THE USER HAS CLICKED THE THREE DOORS SO THAT HE HAS WON
 playDoor = (door) => {
   numClosedDoors --;
   if (numClosedDoors === 0){
     gameOver("win");
-  } else if (isBot(door)) {
+  } else if (door.value === "bot") {
     gameOver("lose");
   }
 }
+// ACTION TO REVEAL THE NEW IMAGE AFTER CLICKING THE DOOR
+actionOnClick = (door, openDoor, clickNum) => {
+  if (currentlyPlaying && clickNum === 1) {
+    door.src = openDoor;
+    playDoor(door);
+  }
+}
 
-doorImage1.onclick = () => {
-  if (currentlyPlaying && !isClicked(doorImage1)){
-  doorImage1.src = openDoor1;
-  playDoor(doorImage1);
+// RESET THE GAME AFTER CLICKING THE START BUTTON
+startRound = () => {
+  door1.src = images[3];
+  door2.src = images[3];
+  door3.src = images[3];
+  clicked1 = 0;
+  clicked2 = 0;
+  clicked3 = 0;
+  numClosedDoors = 3;
+  currentlyPlaying = true;
+  startButton.innerHTML = "Good Luck";
+  currentScore.innerHTML = currentNum;
+  if (bestNum > bestScore.innerHTML) {
+    bestScore.innerHTML = bestNum;
+  };
 }
-}
-doorImage2.onclick = () => {
-  if (currentlyPlaying && !isClicked(doorImage2)){
-    doorImage2.src = openDoor2;
-    playDoor(doorImage2);}
-}
-doorImage3.onclick = () => {
-  if (currentlyPlaying && !isClicked(doorImage3)){
-    doorImage3.src = openDoor3;
-    playDoor(doorImage3);}
-}
+
+// ## CALL TO FUNCTIONS
+randomChoreDoorGenerator();
+
+door1.onclick = () =>{
+  clicked1++;
+  actionOnClick (door1, openDoor1, clicked1);
+} 
+door2.onclick = () =>{
+  clicked2++;
+  actionOnClick (door2, openDoor2, clicked2);
+
+} 
+door3.onclick = () =>{
+  clicked3++;
+  actionOnClick (door3, openDoor3, clicked3);
+
+} 
 
 startButton.onclick = () => {
   if (!currentlyPlaying){
     startRound();
+    randomChoreDoorGenerator();
   }
-}
-
-startRound = () => {
-  doorImage1.src = closedImage;
-  doorImage2.src = closedImage;
-  doorImage3.src = closedImage;
-  numClosedDoors = 3;
-  currentlyPlaying = true;
-  startButton.innerHTML = "Good Luck";
-  currentNum ++;
-  currentScore.innerHTML = currentNum;
 }
